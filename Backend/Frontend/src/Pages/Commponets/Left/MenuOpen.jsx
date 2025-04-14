4e86c4244574b2e85a439cf608ec7ab6cb4df364
Harsh Suryavanshi <Harsh-Dev1602@users.noreply.github.com>
2025-04-14 11:31:14 +0530

    Chat-Application-25.0.0.2.2

--- Backend/Frontend/src/Pages/Commponets/Left/MenuOpen.jsx
+++ Backend/Frontend/src/Pages/Commponets/Left/MenuOpen.jsx
@@ -25,21 +25,23 @@ function MenuOpen() {
   };
   return (
     <>
-      <div className="animate__animated animate__fadeInLeft w-full mx-auto z-50 lg:mx-0 h-screen  lg:w-[29.9%] text-center fixed top-[60px]  left-0  bg-[#f9f7ff] rounded-t-4xl  py-[20px] flex justify-center items-center flex-col gap-5">
+     <div style={{ minHeight: "100vh" }} className="w-full  bg-white custom-scrollbar ">
+      <div style={{ maxHeight: "100vh" }} className="animate__animated animate__fadeInLeft w-full  z-50 lg:mx-0 h-screen  lg:w-[29.9%] text-center fixed top-[65px] py-[100px]  left-0  bg-[#f9f7ff] rounded-t-4xl flex justify-center items-center flex-col gap-5 overflow-y-auto">
 
 
         <div className=' rounded-full  p-4 bg-[#cccc]'>
           <FaUserAlt className=' text-[#848484] text-2xl md:text-4xl ' />
         </div>
 
-        <div className=" p-4 rounded-3xl bg-[#9f9f9f23] flex flex-col gap-2 ">
+        <div className=" p-2 rounded-xl bg-[#9f9f9f23] flex flex-col gap-2 ">
           <span className='text-2xl font-semibold 2xl:text-4xl'>{authUser.user.fullname}</span>
-          <span className=' text-md lg:text-xl text-[#7747ff] font-semibold'>{authUser.user.email}</span>
+          <span className=' text-md xl:text-xl text-[#7747ff] font-semibold'>{authUser.user.email}</span>
         </div>
 
         <button className=' rounded-sm bg-red-600 py-2 px-4 text-white text-xl shadow shadow-[#ccc] cursor-pointer hover:bg-red-400 transition-all duration-1000'
           onClick={handleLogout}
         >Log Out</button>
+      </div>
       </div>
     </>
   )
