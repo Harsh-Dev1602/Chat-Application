b011957d765ce2c8ee5b37f95040a7b2646a9073
Harsh Suryavanshi <Harsh-Dev1602@users.noreply.github.com>
2025-04-18 22:45:55 +0530

    Chat-Application-25.0.0.2.2

--- Backend/Frontend/src/Pages/Loading/Loading1.jsx
+++ Backend/Frontend/src/Pages/Loading/Loading1.jsx
@@ -3,9 +3,9 @@ import "../Loading/Loading.css";
 function Loading1() {
     return (
         <>
-            <div style={{ minHeight: "calc( 100vh - 190px )" }}
+            <div style={{ minHeight: "calc( 100vh - 150px )" }}
                 className="w-full  bg-white flex justify-center items-center">
-                <div style={{ maxHeight: "calc( 100vh - 190px )" }} >
+                <div style={{ maxHeight: "calc( 100vh - 150px )" }} >
                     <div className="loader"></div>
                 </div>
             </div>
