1a33040dd2d3c2aa2da59a4775671b347e090252
Harsh Suryavanshi <178704213+Harsh-Dev1602@users.noreply.github.com>
2025-04-21 02:01:22 +0530

    Update server.js

--- Backend/SocketIO/server.js
+++ Backend/SocketIO/server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://chat-application-lime-six.vercel.app/",
+    origin: "https://chat-application-a5mv.onrender.com",
     methods: ["GET", "POST"],
   },
 });
